59714e148b86d52c2d83d4462e4fadfdfc46d5c5
Pedro Martinez de Aguirre <pedromartinezdeaguirrefernande@servexternos.santandertecnologia.com.ar>
2022-04-30 15:37:30 -0300

    Agregado de parametros por consola

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -7,9 +7,21 @@
 #include "consola.h"
 
 
-int main(void)
-{
-	const char* filename = "identificadores.txt";
+int main(int argc, char **argv) {
+	char* filePath;
+	char* fileSize;
+	t_log* logger = log_create("console.log", "console", true, LOG_LEVEL_DEBUG);
+	// tomar los elementos por parámetros. Se deben recibir:
+	// 1: nombre archivo
+	// 2: tamaño archivo
+	if(argc < 3){
+		log_info(logger, "No se recibieron los param correctos");
+		return EXIT_FAILURE;
+	}
+	filePath = argv[1];
+	fileSize = argv[2];
+
+	const char* filename = filePath;
 
 	FILE* input_file = fopen(filename, "r");
 	if (!input_file)
