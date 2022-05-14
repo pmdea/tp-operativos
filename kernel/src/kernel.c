1053ee7a017529528bea2cabb1ffcd637a3d0722
Daniel Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-05-13 23:37:48 -0300

    agregadas varias cosas al kernel, esperando la parte de envio de paquete de consola

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -5,11 +5,131 @@
  *      Author: pmdea
  */
 
-#include<commons/log.h>
-#include<commons/string.h>
-#include<commons/config.h>
+#include "kernel.h"
 
-int main(void)
-{
+#define PUERTO 8000
+#define IP "127.0.0.1"
+
+void inicializar_direccion_kernel(struct sockaddr_in direccion_kernel);
+void bindear_kernel(int kernel, void* direccion_kernel, t_log* log_kernel);
+void escuchar(int kernel, t_log* log_kernel);
+void procesar_entradas_de_consolas(int kernel, t_log* log_kernel);
+bool handshake(int consola);
+void recibir_datos(int consola);
+void recibir_paquete(int consola, t_paquete *paquete);
+
+void terminar(int kernel, struct sockaddr_in direccion_kernel);
+
+int main(void) {
+	//PROCESOS
+	t_queue *procesos_bloqueados = queue_create();
+	t_queue *procesos_ready = queue_create();
+	t_list *procesos_nuevos = list_create();
+
+
+	//INICIAR KERNEL
+	t_log* log_kernel = log_create("log_kernel.log", "KERNEL", 1,
+			LOG_LEVEL_DEBUG);
+
+	struct sockaddr_in direccion_kernel;
+	inicializar_direccion_kernel(direccion_kernel);
+
+	int kernel = socket(AF_INET, SOCK_STREAM, 0);
+	int activado = 1;
+	setsockopt(kernel, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
+	//free(activado);
+
+	//FIN INICIAR KERNEL
+
+	bindear_kernel(kernel, &direccion_kernel, log_kernel);
+
+	escuchar(kernel,log_kernel);
+
+	procesar_entradas_de_consolas(kernel, log_kernel);
+
+	terminar(kernel, direccion_kernel);
+
+	return 0;
+
+	 //CONEXION CON CPU
+	 //CONEXION CON MEMORIA
+}
+void inicializar_direccion_kernel(struct sockaddr_in direccion_kernel) {
+
+	direccion_kernel.sin_family = AF_INET;
+	direccion_kernel.sin_addr.s_addr = INADDR_ANY;
+	direccion_kernel.sin_port = htons(PUERTO);
 
 }
+
+void bindear_kernel(int kernel, void* direccion_kernel, t_log* log_kernel) {
+	if (bind(kernel, (void*) &direccion_kernel, sizeof(direccion_kernel))
+			!= 0) {
+		perror("fallo el bind");
+		log_error(log_kernel, "Fallo el bind\n");
+	} else {
+		log_info(log_kernel, "El kernel fue bindeado con exito\n");
+	}
+}
+
+void escuchar(int kernel, t_log* log_kernel) {
+	listen(kernel, SOMAXCONN);
+	log_info(log_kernel, "El kernel esta escuchando el puerto: %d\n", PUERTO);
+	log_info(log_kernel, "Esperando conexiones...\n");
+}
+
+void procesar_entradas_de_consolas(int kernel, t_log* log_kernel) {
+
+	while (1) {
+		pthread_t hilo;
+		struct sockaddr_in *direccion_consola;
+		uint32_t *direccion_size;
+		int *consola = malloc(sizeof(int));
+		*consola = accept(kernel, (void*) *&direccion_consola, *&direccion_size);
+
+		if (*consola == -1) {
+			 perror("No se pudo aceptar a la consola");
+			 log_error(log_kernel,"No se pudo aceptar a la consola\n");
+			 } else if(handshake(*consola)) {
+				 log_info(log_kernel,"Se conecto una consola");
+				 pthread_create(&hilo, );
+				 //&hilo, NULL, *recibir_datos(*consola), *consola
+				 pthread_detach(hilo);
+			 }
+
+		free(*direccion_size);
+		free(*consola);
+		log_info(log_kernel, "Esperando nueva conexion...\n");
+	}
+}
+
+bool handshake(int consola) {
+	uint32_t *handshake = malloc(sizeof(uint32_t));
+	int aprobado = 0;
+	int rechazado = -1;
+
+	recv(consola, &*handshake, sizeof(uint32_t), MSG_WAITALL);
+	if(*handshake == 1) {
+		send(consola,aprobado,sizeof(aprobado),0);
+		return true;
+	} else {
+		send(consola,rechazado,sizeof(rechazado),0);
+		return false;
+	}
+}
+
+void recibir_datos(int consola) {
+	t_paquete *paquete = crear_paquete();
+	pcb *pbc_proceso;
+	recibir_paquete(consola, &*paquete);
+	armar_pcb(paquete);
+	enviar_pcb(&pbc_proceso);
+	enviar_mensaje_de_finalizacion(consola);
+}
+
+void recibir_paquete(int consola, t_paquete *paquete) {
+	int tamanio = malloc(sizeof(int));
+	free(tamanio);
+}
+
+
---
+++ kernel/src/kernel.h
@@ -0,0 +1,66 @@
+/*
+ * kernel.h
+ *
+ *  Created on: 13 may. 2022
+ *      Author: utnso
+ */
+
+#ifndef SRC_KERNEL_H_
+#define SRC_KERNEL_H_
+
+
+#include <commons/string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <commons/log.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+#include <netdb.h>
+#include <commons/collections/list.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <assert.h>
+#include <readline/readline.h>
+#include <commons/collections/list.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+*/
+
+typedef enum
+{
+	MENSAJE,
+	PAQUETE
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+typedef struct
+{
+	char *id;
+	int tamanio;
+	t_list *instrucciones;
+	t_list *pc;
+	char *tabla_paginas;
+	int estimacion_rafaga;
+} pcb;
+#endif /* SRC_KERNEL_H_ */
