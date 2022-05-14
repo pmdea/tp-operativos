75f75950d1d033d851c62604f228469d67270728
Santiago Montero <smontero@frba.utn.edu.ar>
2022-05-14 12:45:57 -0300

    Agrego mas validaciones al parser

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -23,32 +23,40 @@ int main(int argc, char **argv) {
 
 	const char* filename = filePath;
 
-	FILE* input_file = fopen("identificadores.txt", "r");
+	FILE* input_file = fopen(filename, "r");
+	//FILE* input_file = fopen("identificadores.txt", "r"); // SI QUERES PROBAR SIN PASAR POR PARAM EL NOMBRE DEL ARCHIVO
 	if (!input_file)
 		exit(EXIT_FAILURE);
 
 	char *contents = NULL;
-	char list_a[] = "";
 	size_t len = 0;
 
+	//t_list* identificadores_list = list_create();
+
 	while (getline(&contents, &len, input_file) != -1){
+
 		char **identificador;
 		char **paramA;
 		char **paramB;
+
 		asignar_valores(contents, &identificador, &paramA, &paramB);
-		/*
-		printf("identificador= %s", identificador);
-		printf("numero1= %s", paramA);
-		printf("numero2= %s", paramB);
-		*/
-		printf("RESULTADOS= %i", validar_identificadores(identificador, paramA, paramB));
 
-		//strcat(list_a ,contents);
+		if(validar_identificadores(identificador, paramA, paramB) == 0){
+			printf("Error en el identificador %s", identificador);
+			exit (EXIT_FAILURE);
+		}
+		// ESTO HAY Q MANDAR --> identificador, paramA, paramB
+
+
+
+
+		//list_add(identificadores_list, element);
+
 	}
-	fclose(input_file);
-	//printf("%s", list_a); // enviar a kernel
-	free(contents);
+	printf("Lei y parsee todo como un campeon");
 
+	fclose(input_file);
+	free(contents);
 	exit(EXIT_SUCCESS);
 
 
--- consola/src/consola.h
+++ consola/src/consola.h
@@ -12,94 +12,71 @@
 #include<stdlib.h>
 #include<commons/log.h>
 #include<commons/string.h>
+#include<commons/collections/list.h>
 #include<commons/config.h>
 #include <string.h>
 
-char *identificadores[6] = {"NO_OP","I/O","READ","WRITE","COPY","EXIT"};
+//char *identificadores[6] = {"NO_OP","I/O","READ","WRITE","COPY","EXIT"};
 
 void asignar_valores(char *string, char **param1, char **param2, char **param3){
 
 	char** split = string_split(string, " ");
+
 	*param1 = split[0];
-	*param2 = split[1];
-	*param3 = split[2];
 
-	/* Version facha
-
-	 char *token;
-    int j;
-
-    for (j = 1; ; j++, string = NULL) {
-        token = strtok(string," ");
-        if (token == NULL)
-            break;
-        if( j == 1){
-        	//strcpy(param1, token);
-        	*param1 = token;
-
-        }else if( j ==2 ){
-        	//strcpy(param2, token);
-        	*param2 = token;
-        }else{
-        	if( token != NULL){
-        		//strcpy(param3,token);
-        		*param3 = token;
-        	}
-        }
-        //printf("%d: %s\n", j, token);
-    }
-
-    //exit(EXIT_SUCCESS);
-    */
-
+	if(split[1]){
+		*param2 = atoi(split[1]);
+	}else{
+		*param2 = NULL;
+	}
+	if(split[2]){
+		*param3 = atoi(split[2]);
+	}else{
+		*param3 = NULL;
+	}
 }
 
 
 int validar_identificadores(char *string, char **param, char **param2){
 
 	if (strcmp(string,"NO_OP") == 0){
-		if(param != NULL  && param2 == NULL){
-
+		if((param || param == 0) && param2 == NULL){
 			return 1;
 		}
 		return 0;
 	}
 
 	if (strcmp(string,"I/O") == 0){
-		if(param != NULL  && param2 == NULL){
+		if((param || param == 0) && param2 == NULL){
 			return 1;
 		}
 		return 0;
 	}
 
 	if (strcmp(string,"READ") == 0){
-		if(param != NULL  && param2 == NULL){
+		if((param || param == 0) && param2 == NULL){
 			return 1;
 		}
 		return 0;
 	}
 
 	if (strcmp(string,"WRITE") == 0){
-		if(param != NULL  && param2 != NULL){
+		if((param || param == 0) && param2){
 			return 1;
 		}
 		return 0;
 	}
 
 	if (strcmp(string,"COPY") == 0){
-		if(param != NULL  && param2 != NULL){
+		if((param || param == 0 )&& param2){
 			return 1;
 		}
 		return 0;
 	}
 	if (strcmp(string,"EXIT") == 0){
-		printf("identificador= %s", string);
 		if(param == NULL && param2 == NULL){
 			return 1;
 		}
-		return 0;
-	}
-	if (strcmp(string," ") == 0 || strcmp(string,"\n") == 0){
 		return 0;
 	}
 
