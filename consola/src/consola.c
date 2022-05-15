747b839fb05fc50b53790f337e61e7867fb8b31e
Santiago-Montero <smontero@frba.utn.edu.ar>
2022-05-15 17:22:25 -0300

    Se agrega una cola de listas las cuales son las instrucciones con sus parametros

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -6,6 +6,72 @@
  */
 #include "consola.h"
 
+void asignar_valores(char *string, char **param1, int *param2, int *param3){
+
+	char** split = string_split(string, " ");
+
+	*param1 = split[0];
+
+	if(split[1]){
+		*param2 = atoi(split[1]);
+	}else{
+		*param2 = -1;
+	}
+	if(split[2]){
+		*param3 = atoi(split[2]);
+	}else{
+		*param3 = -1;
+	}
+}
+
+
+int validar_identificadores(char *string, int *param, int *param2){
+
+	if (strcmp(string,"NO_OP") == 0){
+		if((param || param == 0) && param2 == -1){
+			return 1;
+		}
+		return 0;
+	}
+
+	if (strcmp(string,"I/O") == 0){
+		if((param || param == 0) && param2 == -1){
+			return 1;
+		}
+		return 0;
+	}
+
+	if (strcmp(string,"READ") == 0){
+		if((param || param == 0) && param2 == -1){
+			return 1;
+		}
+		return 0;
+	}
+
+	if (strcmp(string,"WRITE") == 0){
+		if((param || param == 0) && param2){
+			return 1;
+		}
+		return 0;
+	}
+
+	if (strcmp(string,"COPY") == 0){
+		if((param || param == 0 )&& param2){
+			return 1;
+		}
+		return 0;
+	}
+	if (strcmp(string,"EXIT") == 0){
+		if(param == -1 && param2 == -1){
+			return 1;
+		}
+		return 0;
+	}
+
+	//exit (EXIT_FAILURE);
+	return 0;
+}
+
 
 int main(int argc, char **argv) {
 	char* filePath;
@@ -23,40 +89,61 @@ int main(int argc, char **argv) {
 
 	const char* filename = filePath;
 
-	FILE* input_file = fopen(filename, "r");
-	//FILE* input_file = fopen("identificadores.txt", "r"); // SI QUERES PROBAR SIN PASAR POR PARAM EL NOMBRE DEL ARCHIVO
+	//FILE* input_file = fopen(filename, "r");
+	FILE* input_file = fopen("identificadores.txt", "r"); // SI QUERES PROBAR SIN PASAR POR PARAM EL NOMBRE DEL ARCHIVO
 	if (!input_file)
 		exit(EXIT_FAILURE);
 
 	char *contents = NULL;
 	size_t len = 0;
 
-	//t_list* identificadores_list = list_create();
+	Instrucciones* instrucciones ;
+	instrucciones->instrucciones_list = queue_create();
 
 	while (getline(&contents, &len, input_file) != -1){
 
+		t_list * instruccion ;
+		instruccion = list_create();
+
 		char **identificador;
-		char **paramA;
-		char **paramB;
+		int *paramA;
+		int *paramB;
 
 		asignar_valores(contents, &identificador, &paramA, &paramB);
-
 		if(validar_identificadores(identificador, paramA, paramB) == 0){
 			printf("Error en el identificador %s", identificador);
 			exit (EXIT_FAILURE);
 		}
-		// ESTO HAY Q MANDAR --> identificador, paramA, paramB
+
+		// CON STRUCT Instruccion instruccion;
+		// CON STRUCT instruccion.identificador = identificador;
+		// CON STRUCT instruccion.parametros = list_create();
+		// CON STRUCT list_add(instruccion.parametros, paramA);
+		// CON STRUCT list_add(instruccion.parametros, paramB);
+		// CON STRUCT queue_push(instrucciones->instrucciones_list, instruccion); ESTRUCTURA
 
 
+		list_add(instruccion, identificador);
+		if(paramA > 0 ){
+			list_add(instruccion, paramA);
+		}
+		if(paramB > 0 ){
+			list_add(instruccion, paramB);
+		}
 
+		queue_push(instrucciones->instrucciones_list, instruccion);
 
-		//list_add(identificadores_list, element);
+		free(identificador);
 
 	}
-	printf("Lei y parsee todo como un campeon");
 
+	printf("Lei y parsee todo como un campeon");
+	printf("cantidad de instrucciones %i", queue_size(instrucciones->instrucciones_list));
+
+	//queue_destroy_and_destroy_elements(instrucciones->instrucciones_list, );
 	fclose(input_file);
 	free(contents);
+	//free(instrucciones);
 	exit(EXIT_SUCCESS);
 
 
--- consola/src/consola.h
+++ consola/src/consola.h
@@ -13,76 +13,22 @@
 #include<commons/log.h>
 #include<commons/string.h>
 #include<commons/collections/list.h>
+#include<commons/collections/queue.h>
 #include<commons/config.h>
 #include <string.h>
 
 //char *identificadores[6] = {"NO_OP","I/O","READ","WRITE","COPY","EXIT"};
 
-void asignar_valores(char *string, char **param1, char **param2, char **param3){
+/*
+typedef struct {
+	char* identificador;
+	t_list* parametros; // LISTA
+} Instruccion;
+*/
+typedef struct {
+	t_queue* instrucciones_list; // LISTA
+} Instrucciones;
 
-	char** split = string_split(string, " ");
-
-	*param1 = split[0];
-
-	if(split[1]){
-		*param2 = atoi(split[1]);
-	}else{
-		*param2 = NULL;
-	}
-	if(split[2]){
-		*param3 = atoi(split[2]);
-	}else{
-		*param3 = NULL;
-	}
-}
-
-
-int validar_identificadores(char *string, char **param, char **param2){
-
-	if (strcmp(string,"NO_OP") == 0){
-		if((param || param == 0) && param2 == NULL){
-			return 1;
-		}
-		return 0;
-	}
-
-	if (strcmp(string,"I/O") == 0){
-		if((param || param == 0) && param2 == NULL){
-			return 1;
-		}
-		return 0;
-	}
-
-	if (strcmp(string,"READ") == 0){
-		if((param || param == 0) && param2 == NULL){
-			return 1;
-		}
-		return 0;
-	}
-
-	if (strcmp(string,"WRITE") == 0){
-		if((param || param == 0) && param2){
-			return 1;
-		}
-		return 0;
-	}
-
-	if (strcmp(string,"COPY") == 0){
-		if((param || param == 0 )&& param2){
-			return 1;
-		}
-		return 0;
-	}
-	if (strcmp(string,"EXIT") == 0){
-		if(param == NULL && param2 == NULL){
-			return 1;
-		}
-		return 0;
-	}
-
-	//exit (EXIT_FAILURE);
-	return 0;
-}
 
 
 #endif /* CONSOLA_H_ */
