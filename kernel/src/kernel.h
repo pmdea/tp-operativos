3daaa967ce7fa50c41fcf2434af15e2ddac75b2a
Daniel Aizcorbe <daizcorbe@frba.utn.edu.ar>
2022-05-30 01:09:33 -0300

    Agregado:\n >funciones para enviar y recibir mensajes\n >funciones para deserializar mensajes y procesos

--- kernel/src/kernel.h
+++ kernel/src/kernel.h
@@ -10,6 +10,7 @@
 
 
 #include <commons/string.h>
+#include <commons/config.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <commons/log.h>
@@ -26,8 +27,10 @@
 
 typedef enum
 {
+	HANDSHAKE,
+	ENVIO_DATOS,
 	MENSAJE,
-	PAQUETE
+	CONFIRMACION
 }op_code;
 
 typedef struct
@@ -42,6 +45,22 @@ typedef struct
 	t_buffer* buffer;
 } t_paquete;
 
+typedef struct {
+	int tamanio_id;
+	char* identificador;
+	t_queue* parametros;
+}t_instruccion;
+
+typedef struct {
+	int tamanio_proceso;
+	t_queue* instrucciones;
+}t_proceso;
+
+typedef struct {
+	int tamanio_mensaje;
+	char* mensaje;
+}t_mensaje;
+
 typedef struct
 {
 	char *id;
@@ -51,4 +70,7 @@ typedef struct
 	char *tabla_paginas;
 	int estimacion_rafaga;
 } pcb;
+
 #endif /* SRC_KERNEL_H_ */
+
+
