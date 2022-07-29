40a175f3d4e2a4d2570f9c41409ffc637dc6c442
Pedro Martinez de Aguirre <pedromartinezdeaguirrefernande@servexternos.santandertecnologia.com.ar>
2022-07-29 19:46:01 -0300

    coment

--- memoria/memoria_main.c
+++ memoria/memoria_main.c
@@ -1,7 +1,6 @@
 #include "includeh/main.h"
 
 int main(int argc, char **argv){
-//git commit -m "Cambié que solo haya delays para swap cuando hay swapeo de pag, agregado del
 
 	char* path_config;
 	if(argc < 2){
